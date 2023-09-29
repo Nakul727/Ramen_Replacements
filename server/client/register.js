4019a782def50ae9e99c0adf784c5e95aaeade07
Nakul Bansal <nakulbansal727@gmail.com>
2023-09-29 00:14:07 -0700

    Added CORS package, fixed registeration
    CORS package is used for cross-origin requests, that is, frontend
    running on different port can contact the API. CORS allows to restrict
    what port connect to API is allowed.

--- server/client/register.js
+++ server/client/register.js
@@ -6,6 +6,10 @@ async function register_user(name, email, pass)
 {
     try 
 	{
+		// Make a POST request to the Go API for creating a user at /acc/create endpoint
+		// if the registeration was successful, display the response from the API
+		// otherwise display an error message and say that profile couldnt be registered
+        
         const response = await fetch("http://localhost:8080/acc/create", {
             method: "POST",
             headers: {
@@ -23,27 +27,32 @@ async function register_user(name, email, pass)
     } 
 	catch (error) 
 	{
+		// If the API couldn't be contacted
+		// display the error message to the User
         displayMessage("registeration-result","API could not be contacted.");
     }
 }
 
 window.onload = () => {
 
-	let submit_button = document.getElementById("register-submit");
+    let submit_button = document.getElementById("register-submit");
 
-	submit_button.onclick = async () => {
-		let name = document.getElementById("register-name").value;
-		let email = document.getElementById("register-email").value;
-		let pass = document.getElementById("register-pass").value;
-		let pass_confirm = document.getElementById("register-pass-confirm").value;
+    submit_button.onclick = async () => {
+        let name = document.getElementById("register-name").value;
+        let email = document.getElementById("register-email").value;
+        let pass = document.getElementById("register-pass").value;
+        let pass_confirm = document.getElementById("register-pass-confirm").value;
 
+        // make sure that the fields are not empty (*)
+        // password encryption and user authentication - JWT tokens
+
+        // Check if the passwords match
         if (pass !== pass_confirm) {
             displayMessage("registeration-result","Passwords do not match.");
             return;
         } else {
             hideMessage("registeration-result");
         }
-		
         register_user(name, email, pass);
-    }
-}
+    };
+};
