2eb287bef0b87bc5686c3fddd75cb753256f90f4
Nakul Bansal <nakulbansal727@gmail.com>
2023-09-28 23:51:37 -0700

    Added register function

--- server/client/register.js
+++ server/client/register.js
@@ -17,7 +17,30 @@ function hideMessage(elementId) {
     }
 }
 
-function register_user(name, email, pass) {}
+async function register_user(name, email, pass) 
+{
+    try {
+		// send POST request to API
+        const response = await fetch("acc/create", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ username: name, email: email, password: pass }),
+        });
+
+		// display message according to the response
+        if (response.ok) {
+            const result = await response.text();
+            displayMessage("registeration-result", "Profile Registered");
+        } else {
+            const errorData = await response.json();
+            displayMessage("registeration-result", errorData.error);
+        }
+    } catch (error) {
+        displayMessage("registeration-result","API could not be contacted.");
+    }
+}
 
 window.onload = () => {
 
