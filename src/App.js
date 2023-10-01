919df734ce41655e3bc84cfda6c311f2e881302c
NevilleSFU <ntl11@sfu.ca>
2023-09-30 19:16:41 -0700

    Rewrote some CSS as tailwind

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ function App() {
     <div>
       <div>
           <header>
-              <h2 id="logo">Logo goes here</h2>
-              <h1>Ramen Replacements</h1>
-              <h1>Search bar</h1>
+              <h2 className="float-left text-xl m-5">Logo goes here</h2>
+              <h1 className="text-4xl float-left m-5">Ramen Replacements</h1>
+              <h1 className="text-4xl text-center float-left m-5">Search bar</h1>
           </header>
       </div>
       <aside>
