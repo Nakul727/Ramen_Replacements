6a72f9eddd7816113fe9c5685ba44b583be69e1f
NevilleSFU <ntl11@sfu.ca>
2023-10-04 20:59:27 -0700

    Preparing for connecting to backend

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,12 +1,23 @@
 
 import { Link } from "react-router-dom";
+import {displayMessage, hideMessage} from "./helper.js";
 
 function Login() {
+  function login_check() {
+    let name_or_email = document.getElementById("name").value;
+    let password = document.getElementById("pass").value;
+
+    if (name_or_email === "" || password === "") {
+      displayMessage("registration-result", "Some field(s) are empty.");
+    }
+  }
+
   return (
     <div className="mt-60">
       <main className="h-24 w-3/12 m-auto">
-        <h2 className="text-center">Log In</h2>
+        <h2 className="text-center text-2xl font-bold">Log In</h2>
         <hr className="border-black"/>
+        <br/>
         <section className="inline-block w-40">
           <div className="text-center">
             <label>Email or Username</label>
@@ -16,13 +27,15 @@ function Login() {
           </div>
         </section>
         <section className="inline-block">
-          <input className="border border-solid border-black"></input>
+          <input className="border border-solid border-black" id="name"></input>
           <br/>
-          <input className="border border-solid border-black"></input>
+          <input className="border border-solid border-black" id="pass"></input>
           <br/>
         </section>
         <div className="text-center">
-          <button className="w-20 h-8 m-1 bg-stone-300">Log In</button>
+          <button className="w-20 h-8 m-1 bg-stone-300"
+          onClick={login_check}>Log In</button>
+          <p id="registration-result"></p>
         </div>
         <Link to="/register">
           <button className="text-center w-full">
--- src/components/Register.js
+++ src/components/Register.js
@@ -4,12 +4,10 @@ import {displayMessage, hideMessage} from "./helper.js";
 
 
 function Register() {
-    async function register_user(name, email, pass) 
-  {
-      try 
-    {
+  async function register_user(name, email, pass) {
+      try {
       // Make a POST request to the Go API for creating a user at /acc/create endpoint
-      // if the registeration was successful, display the response from the API
+      // if the registration was successful, display the response from the API
       // otherwise display an error message and say that profile couldnt be registered
           
           const response = await fetch("http://localhost:8080/acc/create", {
@@ -21,69 +19,79 @@ function Register() {
           });
           if (response.ok) {
               const result = await response.text();
-              displayMessage("registeration-result", "Profile Registered");
+              displayMessage("registration-result", "Profile Registered");
           } else {
               const errorData = await response.json();
-              displayMessage("registeration-result", errorData.error);
+              displayMessage("registration-result", errorData.error);
           }
-      } 
-    catch (error) 
-    {
+      } catch (error) {
       // If the API couldn't be contacted
       // display the error message to the User
-          displayMessage("registeration-result","API could not be contacted.");
+          displayMessage("registration-result","API could not be contacted.");
       }
   }
   
-  window.onload = () => {
-    let submit_button = document.getElementById("register-submit");
+  function submit() {
+    let name = document.getElementById("register-name").value;
+    let email = document.getElementById("register-email").value;
+    let pass = document.getElementById("register-pass").value;
+    let pass_confirm = document.getElementById("register-pass-confirm").value;
 
-    submit_button.onclick = async () => {
-        let name = document.getElementById("register-name").value;
-        let email = document.getElementById("register-email").value;
-        let pass = document.getElementById("register-pass").value;
-        let pass_confirm = document.getElementById("register-pass-confirm").value;
+    // make sure that the fields are not empty (*)
+    // password encryption and user authentication - JWT tokens
+    // Check if the passwords match
+    if (name === "" || email === "" || pass === "" || pass_confirm === "") {
+      displayMessage("registration-result", "Some field(s) are empty.");
+    } else {
+      if (pass !== pass_confirm) {
+          displayMessage("registration-result", "Passwords do not match.");
+          return;
+      } else {
+          hideMessage("registration-result");
+      }
+      register_user(name, email, pass);
+    }
+  }
 
-        // make sure that the fields are not empty (*)
-        // password encryption and user authentication - JWT tokens
-
-        // Check if the passwords match
-        if (pass !== pass_confirm) {
-            displayMessage("registeration-result","Passwords do not match.");
-            return;
-        } else {
-            hideMessage("registeration-result");
-        }
-        register_user(name, email, pass);
-    };
-  };
   
   return (
     <div className="mt-60">
-      <div className="h-24 w-3/12 m-auto">
-        <h2 className="text-xl font-bold">Register</h2>
-        <hr className="border-black"/><br/>
-        <label for="type">Username</label>
-        <input id="register-name" name="name" type="text" 
-        className="mx-1 border border-black border-solid"></input>
-        <br/><br/>
-        <label for="name">Email</label>
-        <input id="register-email" name="email" type="text"
-        className="mx-1 border border-black border-solid"></input>
-        <br/><br/>
-        <label for="name">Password</label>
-        <input id="register-pass" name="pass" type="password"
-        className="mx-1 border border-black border-solid"></input>
-        <br/><br/>
-        <label for="name">Confirm</label>
-        <input id="register-pass-confirm" name="pass-confirm" type="password"
-        className="mx-1 border border-black border-solid"></input>
-        <br/><br/>
-        <button id="register-submit" type="submit" 
-        className="w-20 h-8 m-1 bg-stone-300">Submit</button>
-        <br/><br/>
-        <p id="registeration-result"></p>
+      <main className="h-24 w-3/12 m-auto">
+        <h2 className="text-center text-2xl font-bold">Register</h2>
+        <hr className="border-black"/>
         <br/>
+        <section className="inline-block w-40">
+          <div className="text-center">
+            <label for="type">Username</label>
+            <br/>
+            <label for="name">Email</label>
+            <br/>
+            <label for="name">Password</label>
+            <br/>
+            <label for="name">Confirm</label>
+            <br/>
+          </div>
+        </section>
+        <section className="inline-block">
+          <input id="register-name" name="name" type="text" 
+          className="border border-solid border-black"></input>
+          <br/>
+          <input id="register-email" name="email" type="text"
+          className="border border-solid border-black"></input>
+          <br/>
+          <input id="register-pass" name="pass" type="password"
+          className="border border-solid border-black"></input>
+          <br/>
+          <input id="register-pass-confirm" name="pass-confirm" type="password"
+          className="border border-solid border-black"></input>
+          <br/>
+        </section>
+        <div className="text-center">
+          <button type="submit" className="w-20 h-8 m-1 bg-stone-300"
+          onClick={submit}>Submit</button>
+          <p id="registration-result"></p>
+        </div>
+
         <Link to="/login">
           <button className="text-center w-full">
             <p className="m-1">Already have an account? Log In</p>
@@ -95,7 +103,7 @@ function Register() {
             <p className="m-2">Cancel</p>
           </button>
         </Link>
-      </div>
+      </main>
     </div>
   );
 }
