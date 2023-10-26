fd7f16c0331fc7daf996cfa0949543702d161017
Nakul Bansal <nakulbansal727@gmail.com>
2023-10-26 00:55:22 -0700

    Moved env check to main.go

--- server/main.go
+++ server/main.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 
 	"server/api"
 	"server/utils"
@@ -13,6 +14,12 @@ import (
 func main() {
 	gin.SetMode(gin.ReleaseMode)
 	r := gin.Default()
+
+	// Make sure environment variables can be loaded
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error Loading .env variables")
+	}
 
 	// connect to the database
 	db, err := utils.InitDB()
