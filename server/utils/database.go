a1539be064aee1d05e277cbfc2358b2fbbf92946
Nakul Bansal <nakulbansal727@gmail.com>
2023-10-26 17:03:06 -0700

    Changes for deploying

--- server/utils/database.go
+++ server/utils/database.go
@@ -5,16 +5,10 @@ import (
 	"log"
 	"os"
 
-	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
 )
 
 func InitDB() (*sql.DB, error) {
-	// Make sure environment variables can be loaded
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error Loading .env variables")
-	}
 
 	// Connect to the server
 	dns := os.Getenv("DB_INFO")
