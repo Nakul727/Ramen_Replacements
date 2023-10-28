caa7f868bc75bbd044f67de9ab0bb467d877ebbd
NevilleSFU <ntl11@sfu.ca>
2023-10-27 17:05:44 -0700

    Fixing import links and headers

--- client/src/pages/Recipe.js
+++ client/src/pages/Recipe.js
@@ -3,6 +3,7 @@ import '../styles/Recipe.css';
 import { Link } from "react-router-dom";
 import { Footer } from "../components/footer.js";
 import { useParams } from 'react-router-dom';
+import { Header, Logo_Name, Links} from '../components/header.js';
 import { displayMessage} from "../components/helper.js";
 import menu_img from "../assets/menu.png";
 import logo_img from "../assets/logo.png";
@@ -29,25 +30,17 @@ function Recipe() {
     }
 
     const recipe = getRecipeDetail(recipeID);
+
+    const header_linkData = [
+        { to: '/explore', text: 'Explore' },
+        { to: '/login', text: 'Login' },
+    ];
       
     return (
         <div>
-            <header className="bg-zinc-300 flex items-center justify-between p-5">
-                <div className="flex items-center mx-5">
-                <Link to="/">
-                    <img src={logo_img} className="h-14 w-auto mx-4" alt="Logo" />
-                </Link>
-                <h1 className="text-black text-4xl ml-2">Ramen Replacements</h1>
-                </div>
-
-                <div className="flex items-center mx-5">
-                <Link to="/login">
-                    <button className="bg-gray-500 hover:bg-gray-600 text-white py-2 px-4 mr-10">
-                    <p className="text-xl">LOGIN</p>
-                    </button>
-                </Link>
-                <img src={menu_img} className="h-12 w-12" alt="Menu" />
-                </div>
+            
+            <header>
+                <Header leftChildren={<Logo_Name />} rightChildren={<Links linkData={header_linkData} />} />
             </header>
 
             <div className="h-[35vw]">
