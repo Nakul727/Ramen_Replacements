25f540bfe73d576abbe4a050a8c2e88f39fbd0b5
Nakul Bansal <nakulbansal727@gmail.com>
2023-10-29 20:38:26 -0700

    Merge Conflicts

--- client/src/components/ExploreRecipes.js
+++ client/src/components/ExploreRecipes.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { displayMessage} from "./helper.js"
+import { displayMessage } from "./helper.js"
 import { Link } from "react-router-dom";
 import "../styles/Home.css";
 
@@ -8,21 +8,28 @@ function RecipeCard(rec) {
     const link = "recipe/" + String(rec.UserID);
     return (
         <Link to={link} className="recipe-card">
-            <p> {rec.Title} </p>
+            <h3 className="text-lg md:text-2xl pt-4"> {rec.Title} </h3>
             <div className="picture-container">
                 <img className="recipe-pic" src={rec.Picture} alt={`alt${rec.Title}`} />
             </div>
-            <div className="text-container">
-                <p>{rec.Description}</p>
+            <div className="pb-4">
+                <p className="text-md md:text-lg">{rec.Description}</p>
             </div>
         </Link>
     );
 }
 
 function getRecipeData() {
+    
     async function getData() {
-        try{
-            const response = await fetch("http://localhost:8080/recipe/latest");
+        try {
+            const backendApi = process.env.REACT_APP_BACKEND;
+            const response = await fetch(`${backendApi}/recipe/latest`, {
+                method: "GET",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+            });
             if (response.ok) {
                 return response.json();
             } else {
@@ -30,7 +37,7 @@ function getRecipeData() {
                 displayMessage("error: ", error.error);
                 return error;
             }
-        } catch(error) {
+        } catch (error) {
             displayMessage("500 internal server error", "there was an error contacting the server")
             return response.json();
         }
@@ -40,7 +47,7 @@ function getRecipeData() {
 }
 
 
-function HomeRecipes() {
+function ExploreRecipes() {
     const [recipeData, setRecipeData] = useState([]);
 
     useEffect(() => {
@@ -69,4 +76,4 @@ function HomeRecipes() {
     );
 }
 
-export {HomeRecipes};
+export { ExploreRecipes };
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -3,7 +3,6 @@ import { Header, Logo_Name, Links} from '../components/header.js';
 import { Footer } from "../components/footer.js"
 import { HomeRecipes } from "../components/HomeRecipes.js"
 
-
 function Home() {
 
   const header_linkData = [
@@ -18,14 +17,33 @@ function Home() {
         <Header leftChildren={<Logo_Name />} rightChildren={<Links linkData={header_linkData} />} />
       </header>
       
-      <div className="mt-32">
-        <p class="text-center text-3xl  mt-10">Welcome to Ramen_Replacements</p>
-        <p class="text-xl mt-10 ml-5">Here are the latest recipes:</p>
-        <div>
-          <HomeRecipes/>
+      <div className='body_sections'>
+        <div className="section1" style={{ backgroundColor: 'lightblue', height: '300px' }}>
+
+          {/* Short description of the platform 
+          Call to action sign up button
+          Image on the right */}
+
+        </div>
+        <div className="section2" style={{ backgroundColor: 'lightgreen', height: '300px' }}>
+
+          {/* Why ramen replacements?
+          - Who this platform is for. 
+          Explore button */}
+          
+        </div>
+        <div className="section3" style={{ backgroundColor: 'lightpink', height: '300px' }}>
+
+          {/* Features
+          - Create your own custom recipies!
+            - add ingredients, add instructions, enjoy!
+          - Explore the Top latest recipies (pictures of the highest rated and latest recipies)
+          - Share your recipies with the world!
+          */}
+
         </div>
       </div>
-      
+
       <footer>
         <Footer/>
       </footer>
