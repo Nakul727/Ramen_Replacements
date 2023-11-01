78e266b5bdfd039d9d552ad46f998b809f1ea253
SimonGCP <simon.g.purdon@gmail.com>
2023-10-31 23:05:08 -0700

    removed placeholders

--- client/src/pages/Explore.js
+++ client/src/pages/Explore.js
@@ -6,7 +6,17 @@ import { ExploreRecipes } from '../components/ExploreRecipes';
 // if the user is not logged in, clicking on a recipe promopts to login and redirects
 // if the user is logged in, they can click on any recipe and view/rate i
 
-
+// TODO
+// section 1:
+/*  Welcome to the explore page!*/
+// section 2:
+/* Div for welcome message to the explore page and what the page is about
+           Here you can find ....*/
+// section 3
+/* Div for the results of the filtering and recipies 
+          Get the top 100 highest rated recipies from the database
+          Display them here in a grid like manner, users can click on it
+          It gets routed to the recipe's unique card. */
 function Explore() {
 
   return (
@@ -14,33 +24,6 @@ function Explore() {
       <header>
         <Header/>
       </header>
-
-      <div className='body_sections'>
-        <div className="section1" style={{ backgroundColor: 'lightblue', height: '300px' }}>
-          {/*  Welcome to the explore page!*/}
-        </div>
-
-        <div className="section2" style={{ backgroundColor: 'lightgreen', height: '300px' }}>
-          {/* Div for welcome message to the explore page and what the page is about
-           Here you can find ....*/}
-        </div>
-
-        <div className="section3" style={{ backgroundColor: 'lightpink', height: '300px' }}>
-          {/* Div for the filtering bar */}
-        </div>
-
-        <div className="section4" style={{ backgroundColor: 'lightgreen', height: '300px' }}>
-
-          {/* Div for the results of the filtering and recipies 
-          Get the top 100 highest rated recipies from the database
-          Display them here in a grid like manner, users can click on it
-          It gets routed to the recipe's unique card. */}
-
-          {/* When the recipies are rendered, make sure to check that if the user is logged in
-          then only it routes to the recipe card otherwise routes to the hompage */}
-
-        </div>
-      </div>
 
       <div>
         <ExploreRecipes />
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -10,20 +10,30 @@ function Home() {
       <header>
         <Header />
       </header>
+      <br/><br/><br/><br/>
+      <h1>Welcome to Ramen Replacements, your new home for delicious and affordable recipes</h1>
+      <p>
+        Here you can view, rate and comment on others' recipes, as well as upload your own. 
+        You can get started by making an account and logging in, then check out the Explore page to see other recipes, 
+        or upload a recipe of your own.
+      </p>
+      <>
       
       <div className='body_sections'>
-        <div className="section1" style={{ backgroundColor: 'lightblue', height: '300px' }}>
+        <div className="section1" style={{ backgroundColor: 'white', height: '675px' }}>
 
           {/* Short description of the platform 
           Call to action sign up button
           Image on the right */}
 
         </div>
+      </div>
+      {/*
         <div className="section2" style={{ backgroundColor: 'lightgreen', height: '300px' }}>
 
           {/* Why ramen replacements?
           - Who this platform is for. 
-          Explore button */}
+          Explore button }
           
         </div>
         <div className="section3" style={{ backgroundColor: 'lightpink', height: '300px' }}>
@@ -33,10 +43,12 @@ function Home() {
             - add ingredients, add instructions, enjoy!
           - Explore the Top latest recipies (pictures of the highest rated and latest recipies)
           - Share your recipies with the world!
-          */}
-
+          }
+        }
         </div>
       </div>
+      */}
+      </>
 
       <footer>
         <Footer/>
