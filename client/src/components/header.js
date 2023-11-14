6dc6aab385c7d4d6f230923c00efb3be7aa7b591
Nakul Bansal <nakulbansal727@gmail.com>
2023-11-13 20:15:04 -0800

    Fix bug

--- client/src/components/header.js
+++ client/src/components/header.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 import logo_img from '../assets/logo.png';
 import dropdown_img from '../assets/dropdown.png';
 import { useAuth } from '../AuthContext.js';
-import { getUserInfo } from './userInfo';
+import { getUserInfo } from './userInfo.js';
 import { useNavigate } from 'react-router-dom';
 
 const buttonStyles = "block mr-2 py-3 px-4 text-gray-900 rounded-xl hover:bg-gray-100 md-hover-bg-transparent transition-all duration-200 ease-in-out";
